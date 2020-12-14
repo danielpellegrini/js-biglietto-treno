8e7f8ef90482d023cae245d5bb3f572276282b8c
Daniel <danielpellegrini87@gmail.com>
2020-12-14 16:55:29 +0100

    BONUS folder added
    test with html css java (failed)

---
+++ BONUS/js/script-bonus.js
@@ -0,0 +1,21 @@
+
+
+var km = parseInt(document.getElementById("km").innerHTML);
+var userAge = parseInt(document.getElementById("userage").innerHTML);
+var price = (km * 0.21)
+
+
+if (userAge < 18) {
+  console.log('User age ' + userAge + ' -> underage user. 20% discount');
+  ticketPrice = (price - (price * 20) / 100)
+
+} else if (userAge > 64) {
+  console.log('User age ' + userAge + ' -> over 65 user. 40% discount');
+  ticketPrice = (price - (price * 40) / 100)
+
+} else  {
+  console.log('User age ' + userAge + ' -> adult user. No discount provided.');
+  ticketPrice = price
+}
+
+document.getElementById("print").innerHTML ="Your ticket price is: " + ticketPrice + '€';
